ca1ba4c7f099d27d57895191b068806c586f338c
Mohammad Yasin <mohammad.yasin@students.makeschool.com>
2021-02-19 12:16:42 -0700

    random commit

--- hero.py
+++ hero.py
@@ -1,20 +1,66 @@
+from ability import Ability
+from armor import Armor
 import random
+
 
 class Hero:
     def __init__(self, name, starting_health = 100):
         self.name = name
         self.starting_health = starting_health
         self.current_health = starting_health
+        self.abilities = list()
+        self.armors = list()
+
+    def add_ability(self, ability):
+        self.abilities.append(ability)
+        # using append method to add ability objects to the list
+
+    def add_armor(self, armor):
+        self.armors.append(armor)
+
+    def attack(self):
+        total_damage = 0
+        # looping through all of the hero's abilities
+        # and returning the total damage
+        for ability in self.abilities:
+            total_damage += ability.attack()
+        return total_damage
+
+    def defend(self):
+        total_block = 0
+        for armor in self.armors:
+            total_block -= armor.defend()
+        return total_block
+
+    def take_damage(self, damage):
+        self.defense -= damage
+        return self.current_health - self.defense
+
+    def is_alive(self):
+        pass
 
     def fight(self, opponent):
-        winner = random.choice(self.name)
-        return f'{winner} Won!'
+        winner = random.choice([self, opponent])
+        return f'{winner.name} Won!'
 
 
 if __name__ == "__main__":
 
-    my_hero = Hero("Grace Hopper", 200)
-    hero1 = Hero("Wonder Woman")
-    hero2 = Hero("Dumbledore")
+    # my_hero = Hero("Grace Hopper", 200)
+    # hero1 = Hero("Wonder Woman")
+    # hero2 = Hero("Dumbledore")
 
-    hero1.fight(hero2)
+    # print(hero1.fight(hero2))
+    # ability = Ability("Great Debugging", 50)
+    # ability2 = Ability('Dancing', 32)
+    # hero = Hero("Grace Hopper", 200)
+    # hero.add_ability(ability)
+    # hero.add_ability(ability2)
+    hero = Hero("Grace Hopper", 200)
+    shield = Armor("Shield", 50)
+    hero.add_armor(shield)
+    hero.take_damage(50)
+    print(hero.current_health)
+
+    
+    
--- my_dogs.py
+++ my_dogs.py
@@ -2,3 +2,5 @@ import dog
 
 my_dog = dog.Dog('Rex', 'SuperDog')
 my_dog.bark()
+
+my_other_dog = dog.Dog("Annie", "SuperDog")
